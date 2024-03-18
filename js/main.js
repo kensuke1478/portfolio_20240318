e9800742e7f74c35f86f939e8d633f641aad5cf6
kensuke <miotyan_shade@yahoo.co.jp>
2024-03-18 17:29:27 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// ハンバーガーメニュー
+$(function () {
+
+  const hamburgerMenu = document.querySelector('.hamburger-menu');
+  const navMenu = document.querySelector('.nav-menu');
+  hamburgerMenu.addEventListener('click', () => {
+    hamburgerMenu.classList.toggle('active');
+    navMenu.classList.toggle('active');
+  });
+});
+
+
+// アニメーションの作成
+
